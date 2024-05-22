bc97d13c13a4dad0337c58fa109ecad95e7af6e3
andre_hilario <andre.costa@bernoulli.com.br>
2024-05-22 14:29:15 -0300

    create models, migrations, providers

--- app/Providers/AdminPanelProvider.php
+++ app/Providers/AdminPanelProvider.php
@@ -2,13 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+class AdminPanelProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register services.
      */
     public function register(): void
     {
@@ -16,10 +15,10 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-     * Bootstrap any application services.
+     * Bootstrap services.
      */
     public function boot(): void
     {
-        Model::unguard();
+        //
     }
 }
