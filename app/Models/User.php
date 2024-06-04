97135eca6640f8d63720e3431f09751bfb9283a8
andre costa <andre.hcosta@hotmail.com>
2024-06-04 16:31:36 -0300

    publish vendor

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use League\CommonMark\Environment\Environment;
 
 class User extends Authenticatable
 {
